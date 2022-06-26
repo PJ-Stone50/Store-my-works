48a8d367fd1cd4fb7341b20929a207e8607ea400
PJ-Stone50 <pj.webdeveloper01@gmail.com>
2022-06-26 19:49:46 +0700

    Upload SoundEffect repo

---
+++ SoundEffect/src/pages/Signin.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import { Container, Card, Row, Col, Image, Button } from 'react-bootstrap'
+import { Container,ContainerHome } from '../style/layoutStyled'
+import { ButtonContent, Title, SubTitle, BodyText } from '../style/styledElements'
+
+
+export const Signin = () => {
+  return (
+    <Container className='h-screen'>
+      <Title>SignIn</Title>
+
+      
+    </Container>
+  )
+}
+
+
+
